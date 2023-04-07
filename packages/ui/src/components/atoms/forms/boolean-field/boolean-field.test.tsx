8c07ed7d7aa89b102a30037ccb6d0ccd7fb3fe58
Armand Philippot <git@armandphilippot.com>
2023-04-07 18:00:48 +0200

    test(ui): fix warning about onChange on boolean field tests

--- packages/ui/src/components/atoms/forms/boolean-field/boolean-field.test.tsx
+++ packages/ui/src/components/atoms/forms/boolean-field/boolean-field.test.tsx
@@ -2,14 +2,34 @@ import { render, screen as screenTL } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import { BooleanField } from './boolean-field';
 
+const handleChange = () => {
+  /**
+   * Do nothing.
+   */
+};
+
 describe('boolean field', () => {
   it('renders a checkbox', () => {
-    render(<BooleanField id="checkbox" name="checkbox" type="checkbox" />);
+    render(
+      <BooleanField
+        id="checkbox"
+        name="checkbox"
+        onChange={handleChange}
+        type="checkbox"
+      />
+    );
     expect(screenTL.getByRole('checkbox')).toBeInTheDocument();
   });
 
   it('renders a radio button', () => {
-    render(<BooleanField id="radio" name="radio" type="radio" />);
+    render(
+      <BooleanField
+        id="radio"
+        name="radio"
+        onChange={handleChange}
+        type="radio"
+      />
+    );
     expect(screenTL.getByRole('radio')).toBeInTheDocument();
   });
 });
