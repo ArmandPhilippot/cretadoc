8ad82aca98ca5183be85edc876c18d62a323d686
Armand Philippot <git@armandphilippot.com>
2023-04-17 17:48:17 +0200

    fix(ui): restrict type attribute of Input component
    Checkbox and radio are handle with their own components and range is
    not supported for example. So it is better to let Typescript consumers
    know that only some values are accepted.

--- packages/ui/src/components/atoms/forms/fields/input/input.tsx
+++ packages/ui/src/components/atoms/forms/fields/input/input.tsx
@@ -1,29 +1,39 @@
-import { type ForwardedRef, forwardRef, type InputHTMLAttributes } from 'react';
+import {
+  type ForwardedRef,
+  forwardRef,
+  type InputHTMLAttributes,
+  type HTMLInputTypeAttribute,
+} from 'react';
 import * as styles from '../fields.css';
 
 export type InputProps = Omit<
   InputHTMLAttributes<HTMLInputElement>,
-  'disabled' | 'readonly' | 'required'
-> & {
-  /**
-   * Should the field be disabled?
-   *
-   * @default false
-   */
-  isDisabled?: boolean;
-  /**
-   * Should the field be readonly?
-   *
-   * @default false
-   */
-  isReadOnly?: boolean;
-  /**
-   * Should the field be required?
-   *
-   * @default false
-   */
-  isRequired?: boolean;
-};
+  'disabled' | 'readonly' | 'required' | 'type'
+> &
+  Required<Pick<InputHTMLAttributes<HTMLInputElement>, 'id' | 'name'>> & {
+    /**
+     * Should the field be disabled?
+     *
+     * @default false
+     */
+    isDisabled?: boolean;
+    /**
+     * Should the field be readonly?
+     *
+     * @default false
+     */
+    isReadOnly?: boolean;
+    /**
+     * Should the field be required?
+     *
+     * @default false
+     */
+    isRequired?: boolean;
+    /**
+     * The input type.
+     */
+    type: Exclude<HTMLInputTypeAttribute, 'checkbox' | 'radio' | 'range'>;
+  };
 
 const InputWithRef = (
   {
