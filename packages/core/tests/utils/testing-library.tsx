11e59da36927b4f9eff79074fbc0ddb324316f42
Armand Philippot <git@armandphilippot.com>
2023-05-26 15:14:04 +0200

    test(core): wrap each tests with necessary providers

---
+++ packages/core/tests/utils/testing-library.tsx
@@ -0,0 +1,44 @@
+import {
+  render as rtlRender,
+  type RenderOptions,
+} from '@testing-library/react';
+import type { FC, ReactElement, ReactNode } from 'react';
+import { IntlProvider } from 'react-intl';
+import { MemoryRouter } from 'react-router-dom';
+import type { CretadocConfig } from '../../src/types/config';
+import { ConfigProvider } from '../../src/utils/contexts';
+
+type ProvidersConfig = {
+  config?: CretadocConfig;
+  locale?: 'en';
+};
+
+type AllProvidersProps = ProvidersConfig & {
+  children: ReactNode;
+};
+
+const AllProviders: FC<AllProvidersProps> = ({
+  children,
+  config,
+  locale = 'en',
+}) => (
+  <ConfigProvider config={config}>
+    <IntlProvider locale={locale}>
+      <MemoryRouter>{children}</MemoryRouter>
+    </IntlProvider>
+  </ConfigProvider>
+);
+
+type CustomRenderOptions = {
+  providers?: ProvidersConfig;
+  testingLibrary?: Omit<RenderOptions, 'wrapper'>;
+};
+
+const render = (ui: ReactElement, options: CustomRenderOptions = {}) =>
+  rtlRender(ui, {
+    wrapper: (props) => <AllProviders {...props} {...options.providers} />,
+    ...options.testingLibrary,
+  });
+
+export * from '@testing-library/react';
+export { render };
