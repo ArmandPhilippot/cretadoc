16fd2da42bbb6d79daabb68fe24c7ec81908deb3
Armand Philippot <git@armandphilippot.com>
2023-05-26 18:54:52 +0200

    fix(ui): make exported themes readonly
    By using the satisfies operator, Typescript can transform theme values
    to literal types. It can be useful to infer a list of themes ids for
    example.

--- packages/ui/src/themes/library/cretadoc/index.ts
+++ packages/ui/src/themes/library/cretadoc/index.ts
@@ -4,49 +4,49 @@ import { border } from './border';
 import { darkColor, lightColor } from './color';
 import { font } from './font';
 import { icon } from './icon';
-import { shadow } from './shadow';
+import { darkShadow, lightShadow } from './shadow';
 import { size } from './size';
 import { spacing } from './spacing';
 
-const cretadocAuthor: ThemeAuthor = {
+const cretadocAuthor = {
   name: 'Cretadoc',
   website: 'https://github.com/ArmandPhilippot/cretadoc',
-};
+} satisfies ThemeAuthor;
 
-const cretadocLightTokens: ThemeTokens = {
+const cretadocLightTokens = {
   animation,
   border,
   color: lightColor,
   font,
   icon,
-  shadow,
+  shadow: lightShadow,
   size,
   spacing,
-};
+} satisfies ThemeTokens;
 
-export const cretadocLight: Theme = {
+export const cretadocLight = {
   author: cretadocAuthor,
   id: 'cretadoc-light',
   name: 'Cretadoc Light',
   scheme: 'light',
   tokens: cretadocLightTokens,
-};
+} as const satisfies Theme;
 
-const cretadocDarkTokens: ThemeTokens = {
+const cretadocDarkTokens = {
   animation,
   border,
   color: darkColor,
   font,
   icon,
-  shadow,
+  shadow: darkShadow,
   size,
   spacing,
-};
+} satisfies ThemeTokens;
 
-export const cretadocDark: Theme = {
+export const cretadocDark = {
   author: cretadocAuthor,
   id: 'cretadoc-dark',
   name: 'Cretadoc Dark',
   scheme: 'dark',
   tokens: cretadocDarkTokens,
-};
+} as const satisfies Theme;
--- packages/ui/src/themes/library/index.ts
+++ packages/ui/src/themes/library/index.ts
@@ -1,3 +1,8 @@
+import type { ReadonlyDeep } from '@cretadoc/utils';
+import type { Themes } from '../types';
 import { cretadocDark, cretadocLight } from './cretadoc';
 
-export const themes = { cretadocDark, cretadocLight };
+export const themes = {
+  cretadocDark,
+  cretadocLight,
+} satisfies ReadonlyDeep<Themes>;
