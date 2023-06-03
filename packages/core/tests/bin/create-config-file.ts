c1f7cb45f00d4c6b83ec1c17fdd8c76cbc9f6f82
Armand Philippot <git@armandphilippot.com>
2023-06-03 18:09:30 +0200

    chore(core): remove unecessary files in tests dir
    Since we are creating the config file directly in `server.ts` file,
    we do not need the bin directory.

--- packages/core/tests/bin/create-config-file.ts
+++
@@ -1,4 +0,0 @@
-import { ROOT_CONFIG_PATH } from '../utils/constants';
-import { createConfigFile } from '../utils/helpers';
-
-await createConfigFile(ROOT_CONFIG_PATH);
--- packages/core/tests/bin/remove-config-file.ts
+++
@@ -1,4 +0,0 @@
-import { ROOT_CONFIG_PATH } from '../utils/constants';
-import { removeConfigFile } from '../utils/helpers';
-
-await removeConfigFile(ROOT_CONFIG_PATH);
