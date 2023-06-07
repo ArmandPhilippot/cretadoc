1e674da49cd3046d5571f4de29814f89c889d8ce
Armand Philippot <git@armandphilippot.com>
2023-06-07 16:01:41 +0200

    fix(ui): prevent error because of window in useScrollLock hook

--- packages/ui/src/hooks/use-scroll-lock/use-scroll-lock.ts
+++ packages/ui/src/hooks/use-scroll-lock/use-scroll-lock.ts
@@ -1,5 +1,11 @@
-import { type RefObject, useCallback, useEffect, useState } from 'react';
+import type { Nullable } from '@cretadoc/utils';
+import { type RefObject, useCallback, useEffect, useRef, useMemo } from 'react';
 import { useScrollBarWidth } from '../use-scrollbar-width';
+
+type Styles = {
+  overflow: string;
+  paddingRight: string;
+};
 
 /**
  * React hook to lock/unlock the scroll on the body or the given element.
@@ -11,28 +17,57 @@ export const useScrollLock = (
   isScrollLocked = false,
   ref?: RefObject<HTMLElement>
 ) => {
-  const el = ref?.current ?? document.body;
-  const [originalStyle, setOriginalStyle] = useState(el.style.cssText);
   const scrollbarWidth = useScrollBarWidth(ref);
+  const elRef = useRef<Nullable<HTMLElement>>(null);
+  const initialStyles = useRef<Nullable<Styles>>(null);
+  const lockedStyles: Styles = useMemo(() => {
+    return {
+      overflow: 'hidden',
+      paddingRight: `calc(${
+        initialStyles.current?.paddingRight ?? 0
+      } + ${scrollbarWidth}px)`,
+    };
+  }, [scrollbarWidth]);
+
+  const getUnlockedStyles = useCallback((): Styles => {
+    if (!elRef.current)
+      return {
+        overflow: '',
+        paddingRight: '',
+      };
+
+    const computedStyle = window.getComputedStyle(elRef.current);
+    return {
+      overflow: computedStyle.overflow,
+      paddingRight: computedStyle.paddingRight,
+    };
+  }, []);
 
   useEffect(() => {
-    setOriginalStyle(el.style.cssText);
-  }, [el]);
+    elRef.current = ref ? ref.current : document.body;
+    initialStyles.current = getUnlockedStyles();
+  }, [getUnlockedStyles, ref]);
 
   const lockScroll = useCallback(() => {
-    const elOriginalPaddingRight = window.getComputedStyle(el).paddingRight;
-    el.style.paddingRight = elOriginalPaddingRight
-      ? `calc(${elOriginalPaddingRight} + ${scrollbarWidth}px)`
-      : `${scrollbarWidth}px`;
-    el.style.overflow = 'hidden';
-  }, [el, scrollbarWidth]);
+    if (!elRef.current) return;
+
+    elRef.current.style.overflow = lockedStyles.overflow;
+    elRef.current.style.paddingRight = lockedStyles.paddingRight;
+  }, [lockedStyles]);
 
   const unlockScroll = useCallback(() => {
-    el.style.cssText = originalStyle;
-  }, [el, originalStyle]);
+    if (!elRef.current) return;
+
+    elRef.current.style.overflow = initialStyles.current?.overflow ?? '';
+    elRef.current.style.paddingRight =
+      initialStyles.current?.paddingRight ?? '';
+  }, []);
 
   useEffect(() => {
     if (isScrollLocked) lockScroll();
-    else unlockScroll();
+
+    return () => {
+      unlockScroll();
+    };
   }, [isScrollLocked, lockScroll, unlockScroll]);
 };
