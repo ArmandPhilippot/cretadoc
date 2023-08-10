8cb9f1d8680f07b031020a823238bccb065830d5
Armand Philippot <git@armandphilippot.com>
2023-08-10 18:54:29 +0200

    build(api): bump deps

--- packages/api/vitest.config.ts
+++ packages/api/vitest.config.ts
@@ -3,15 +3,16 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    deps: {
-      inline: ['graphql', 'graphql-yoga'], // Prevent graphql resolution errors.
-      fallbackCJS: true, // Prevent duplicate graphql errors
-    },
     include: [
       './src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts}',
       './tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts}',
     ],
     reporters: 'verbose',
+    server: {
+      deps: {
+        fallbackCJS: true, // Prevent duplicate graphql errors
+      },
+    },
     watch: false,
   },
   resolve: {
