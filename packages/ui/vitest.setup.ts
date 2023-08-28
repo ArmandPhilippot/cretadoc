3ff8741579bae9db1f9dd12eb1935af663020ca4
Armand Philippot <git@armandphilippot.com>
2023-08-28 11:51:33 +0200

    build(ui): bump dependencies

--- packages/ui/vitest.setup.ts
+++ packages/ui/vitest.setup.ts
@@ -1,22 +1,7 @@
-/* eslint-disable @typescript-eslint/no-empty-interface */
-/* eslint-disable @typescript-eslint/no-namespace */
-/* eslint-disable @typescript-eslint/consistent-type-definitions */
-import matchers, {
-  type TestingLibraryMatchers,
-} from '@testing-library/jest-dom/matchers';
 import { cleanup } from '@testing-library/react';
-import { afterEach, expect } from 'vitest';
+import { afterEach } from 'vitest';
+import '@testing-library/jest-dom/vitest';
 
 afterEach(() => {
   cleanup();
 });
-
-declare global {
-  namespace Vi {
-    interface JestAssertion<T>
-      extends jest.Matchers<void, T>,
-        TestingLibraryMatchers<T, void> {}
-  }
-}
-
-expect.extend(matchers);
